c8741069be07a8e438c6ae67a04f4597b4b4ddb1
Ayushi Rathore <ayushi.rathore@icorprated.com>
2019-11-11 17:10:58 +0530

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   resources :authors
-  get 'welcome/index'
+  get '/'
   resources :articles do
   	resources :comments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root '/'
 end
